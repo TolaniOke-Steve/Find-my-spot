9047a3758fb6745bc9696ece8d507a40a96d6180
TOLANI OKE-STEVE <emmasam1247@gamil.com>
2025-01-05 13:06:31 -0600

    updates 2025

--- src/script.ts
+++ src/script.ts
@@ -1,7 +1,30 @@
+const express = require('express');
+const path = require('path');
+const app = express();
 
+const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
 
+  app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+  });
+
+  interface Request {
+    // Define the properties you need from the request object
+  }
+
+  interface Response {
+    sendFile: (path: string) => void;
+  }
+
+  app.get("/", (req: Request, res: Response) => {
+    res.sendFile(__dirname + "/login.html");
+  });
 // Function login
 let flag: any = false;
 
